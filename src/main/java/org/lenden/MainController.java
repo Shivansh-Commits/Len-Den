323566ea508f997297e971ec1956b67915448695
Shivansh-Commits <shivanshbhadouria7@gmail.com>
2023-07-18 18:23:02 +0530

    commit

--- src/main/java/org/lenden/MainController.java
+++ src/main/java/org/lenden/MainController.java
@@ -15,8 +15,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
