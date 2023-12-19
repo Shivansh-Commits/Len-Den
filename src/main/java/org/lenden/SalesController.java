12ce75dbf6edd851fe33390f75f635dddead065b
Shivansh-Commits <shivanshbhadouria7@gmail.com>
2023-12-19 22:23:25 +0530

    Additions in Sales Dashboard 1. Filters functionality to display yearly bills. 2. Implemented Search Functionality. Added a combobox to select "searchBy" option and Search Box to write "Search Text"

--- src/main/java/org/lenden/SalesController.java
+++ src/main/java/org/lenden/SalesController.java
@@ -11,9 +11,11 @@ import javafx.scene.layout.BorderPane;
 import org.lenden.dao.DaoImpl;
 import org.lenden.model.Bill;
 import java.net.URL;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Pattern;
 
 
 public class SalesController implements Initializable
@@ -42,6 +44,12 @@ public class SalesController implements Initializable
     ToggleButton monthlyFilterToggleButton;
     @FXML
     ToggleButton quarterlyFilterToggleButton;
+    @FXML
+    ToggleButton yearlyFilterToggleButton;
+    @FXML
+    TextField searchField;
+    @FXML
+    ComboBox<String> searchOptionsComboBox;
 
     ObservableList<Bill> bills = FXCollections.observableArrayList();
     DaoImpl daoImpl = new DaoImpl();
@@ -63,46 +71,170 @@ public class SalesController implements Initializable
             alert.showAndWait();
         }
 
-
-
         //----------------DISPLAYING DEFAULT (TODAY'S BILLS AND SALES) DATA---------------------
 
         displayTodaysBillData(bills);
         todaysFilterToggleButton.setSelected(true);
 
 
+        //-----------------ADDING EVENT LISTENER TO SEARCH FIELD-------------------------
+
+        searchField.textProperty().addListener((observable, searchFieldOldValue, searchFieldNewValue) -> {
+            searchTableBy(searchFieldNewValue);
+        });
+
+
+        //----------------SETTING/ADDING SEARCH OPTIONS IN COMBO BOX---------------------
+
+        ArrayList<String> searchOptions = new ArrayList<>();
+        searchOptions.add("Bill No.");
+        searchOptions.add("Status");
+        searchOptions.add("Mode of Payment");
+        searchOptions.add("Date");
+        searchOptions.add("Grand Total");
+        searchOptionsComboBox.getItems().addAll(searchOptions);
+
 
         //---------------SETTING THE FUNCTIONALITY OF ALL FILTER (TOGGLE) BUTTONS----------------
-        todaysFilterToggleButton.setTooltip(new javafx.scene.control.Tooltip("This button does something cool!"));
+        todaysFilterToggleButton.setTooltip(new javafx.scene.control.Tooltip("Filter Bills generated Today"));
         todaysFilterToggleButton.setOnAction(event -> {
             if (todaysFilterToggleButton.isSelected())
             {
                 displayTodaysBillData(bills);
+                searchField.clear();
             }
         });
 
+        weeklyFilterToggleButton.setTooltip(new javafx.scene.control.Tooltip("Filter Bills of Current Week"));
         weeklyFilterToggleButton.setOnAction(event -> {
             if (weeklyFilterToggleButton.isSelected())
             {
                 displayThisWeeksBillsData(bills);
+                searchField.clear();
             }
         });
 
+        monthlyFilterToggleButton.setTooltip(new javafx.scene.control.Tooltip("Filter Bills of Current Month"));
         monthlyFilterToggleButton.setOnAction(event -> {
             if (monthlyFilterToggleButton.isSelected())
             {
                 displayThisMonthsBillsData(bills);
+                searchField.clear();
             }
         });
 
+        quarterlyFilterToggleButton.setTooltip(new javafx.scene.control.Tooltip("Filter Bills of Current Quarter i.e. Last 3 Months"));
         quarterlyFilterToggleButton.setOnAction(event -> {
             if (quarterlyFilterToggleButton.isSelected())
             {
                 displayQuarterlyBillsData(bills);
+                searchField.clear();
+            }
+        });
+
+        yearlyFilterToggleButton.setTooltip(new javafx.scene.control.Tooltip("Shows current years Bills"));
+        yearlyFilterToggleButton.setOnAction(event -> {
+            if (yearlyFilterToggleButton.isSelected())
+            {
+                displayCurrentYearsBillsData(bills);
+                searchField.clear();
             }
         });
 
     }
+
+    private void searchTableBy(String searchText)
+    {
+        //Null check
+        if(searchOptionsComboBox.getValue() == null)
+        {
+            searchOptionsComboBox.requestFocus();
+            searchOptionsComboBox.show();
+            searchOptionsComboBox.setValue("Status");
+
+            return;
+        }
+
+        // If the search text is empty, display all bills
+        if (searchText == null || searchText.trim().isEmpty())
+        {
+            if (weeklyFilterToggleButton.isSelected())
+                displayThisWeeksBillsData(bills);
+            else if (monthlyFilterToggleButton.isSelected())
+                displayThisMonthsBillsData(bills);
+            else if (quarterlyFilterToggleButton.isSelected())
+                displayQuarterlyBillsData(bills);
+            else if(yearlyFilterToggleButton.isSelected())
+                displayCurrentYearsBillsData(bills);
+            else
+                displayTodaysBillData(bills);
+
+            return;
+        }
+
+        if(searchOptionsComboBox.getValue().equals("Bill No."))
+        {
+
+            // Filter bills based on the Bill No. containing the search text (case insensitive)
+            ObservableList<Bill> billsDisplayedInTable = billsTable.getItems();
+            ObservableList<Bill> filteredBills = billsDisplayedInTable.filtered(bill ->
+                    String.valueOf(bill.getBillnumber()).toLowerCase().contains(searchText.toLowerCase())
+            );
+
+            // Update the TableView with the filtered results
+            billsTable.setItems(filteredBills);
+        }
+        else if(searchOptionsComboBox.getValue().equals("Status"))
+        {
+
+            // Filter bills based on the status containing the search text (case insensitive)
+            ObservableList<Bill> billsDisplayedInTable = billsTable.getItems();
+            ObservableList<Bill> filteredBills = billsDisplayedInTable.filtered(bill ->
+                    bill.getStatus().toLowerCase().contains(searchText.toLowerCase())
+            );
+
+            // Update the TableView with the filtered results
+            billsTable.setItems(filteredBills);
+        }
+        else if(searchOptionsComboBox.getValue().equals("Mode of Payment"))
+        {
+
+            // Filter bills based on the Mode of payment containing the search text (case insensitive)
+            ObservableList<Bill> billsDisplayedInTable = billsTable.getItems();
+            ObservableList<Bill> filteredBills = billsDisplayedInTable.filtered(bill ->
+                    bill.getModeOfpayment().toLowerCase().contains(searchText.toLowerCase())
+            );
+
+            // Update the TableView with the filtered results
+            billsTable.setItems(filteredBills);
+        }
+        else if(searchOptionsComboBox.getValue().equals("Date"))
+        {
+
+            // Filter bills based on the Date containing the search text (case insensitive)
+            ObservableList<Bill> billsDisplayedInTable = billsTable.getItems();
+            ObservableList<Bill> filteredBills = billsDisplayedInTable.filtered(bill ->
+                    bill.getDate().toLowerCase().contains(searchText.toLowerCase())
+            );
+
+            // Update the TableView with the filtered results
+            billsTable.setItems(filteredBills);
+        }
+        else if(searchOptionsComboBox.getValue().equals("Grand Total"))
+        {
+
+            // Filter bills based on the Grand Total containing the search text (case insensitive)
+            ObservableList<Bill> billsDisplayedInTable = billsTable.getItems();
+            ObservableList<Bill> filteredBills = billsDisplayedInTable.filtered(bill ->
+                    String.valueOf(bill.getGrandTotal()).toLowerCase().contains(searchText.toLowerCase())
+            );
+
+            // Update the TableView with the filtered results
+            billsTable.setItems(filteredBills);
+        }
+    }
+
+
 
     public boolean isSameDay(String dateString1, String dateString2)
     {
@@ -121,7 +253,6 @@ public class SalesController implements Initializable
         // Compare day, month, and year components
         return (day1 == day2) && (month1 == month2) && (year1 == year2);
     }
-
     public void displayTodaysBillData(ObservableList<Bill> bills)
     {
         //FILTERING ONLY TODAY'S Bills
@@ -259,7 +390,6 @@ public class SalesController implements Initializable
             return false;
         }
     }
-
     public void displayThisWeeksBillsData(List<Bill> bills)
     {
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd/MM/yyyy", Locale.ENGLISH);
@@ -387,7 +517,6 @@ public class SalesController implements Initializable
         // Compare month and year components
         return (month1 == month2) && (year1 == year2);
     }
-
     public void displayThisMonthsBillsData(ObservableList<Bill> bills)
     {
         // FILTERING ONLY THIS MONTH'S Bills
@@ -520,7 +649,6 @@ public class SalesController implements Initializable
         // Check if the difference is less than or equal to 3
         return monthsDifference >= 0 && monthsDifference <= 2;
     }
-
     public void displayQuarterlyBillsData(ObservableList<Bill> bills)
     {
         // FILTERING ONLY LAST 3 MONTHS' Bills
@@ -632,6 +760,122 @@ public class SalesController implements Initializable
             }
         });
 
+    }
+
+
+    public boolean isSameYear(String dateString1, String dateString2) {
+        // Extract year components from both date strings
+        String[] components1 = dateString1.split(" ")[1].split("/");
+        String[] components2 = dateString2.split(" ")[1].split("/");
+
+        int year1 = Integer.parseInt(components1[2]);
+        int year2 = Integer.parseInt(components2[2]);
+
+        // Compare year components
+        return year1 == year2;
+    }
+
+    public void displayCurrentYearsBillsData(ObservableList<Bill> bills) {
+        // FILTERING ONLY THIS YEAR'S Bills
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd/MM/yyyy");
+        String currentDate = dateFormat.format(new Date());
+
+        ObservableList<Bill> thisYearsBills = FXCollections.observableArrayList();
+        for (Bill bill : bills) {
+            if (isSameYear(bill.getDate(), currentDate)) {
+                thisYearsBills.add(bill);
+            }
+        }
+
+        double netSales = thisYearsBills.stream()
+                .filter(bill -> !bill.getStatus().equals("CANCELLED"))
+                .mapToDouble(Bill::getGrandTotal)
+                .sum();
+        netSalesLabel.setText(String.format("%.2f", netSales));
+
+        double netProfit = thisYearsBills.stream()
+                .filter(bill -> !bill.getStatus().equals("CANCELLED"))
+                .mapToDouble(Bill::getTotal)
+                .sum();
+        netProfitLabel.setText(String.format("%.2f", netProfit));
+
+        long totalOrders = thisYearsBills.stream()
+                .filter(bill -> !bill.getStatus().equals("CANCELLED"))
+                .count();
+        totalOrdersLabel.setText(String.valueOf(totalOrders));
+
+        filterIdentifierLabelA.setText("This Year's");
+        filterIdentifierLabelB.setText("This Year's");
+        filterIdentifierLabelC.setText("This Year's");
+
+        //------------------DISPLAYING BILLS IN TABLE------------------------
+        // Create a cell value factory for the Bill No. column
+        TableColumn<Bill, Integer> billNoCol = new TableColumn<>("Bill No.");
+        billNoCol.setCellValueFactory(new PropertyValueFactory<>("billnumber"));
+        billNoCol.setPrefWidth(100);
+        billNoCol.setStyle("-fx-alignment: CENTER;");
+        billNoCol.setSortType(TableColumn.SortType.DESCENDING);
+
+        // Create a cell value factory for the Date column
+        TableColumn<Bill, String> dateCol = new TableColumn<>("Date");
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        dateCol.setPrefWidth(170);
+
+        // Create a cell value factory for the Grand Total column
+        TableColumn<Bill, Double> grandTotalCol = new TableColumn<>("Grand Total");
+        grandTotalCol.setCellValueFactory(new PropertyValueFactory<>("grandTotal"));
+        grandTotalCol.setPrefWidth(170);
+        grandTotalCol.setStyle("-fx-alignment: CENTER;");
+
+        // Create a cell value factory for the Date column
+        TableColumn<Bill, Double> totalCol = new TableColumn<>("Total");
+        totalCol.setCellValueFactory(new PropertyValueFactory<>("total"));
+        totalCol.setPrefWidth(170);
+        totalCol.setStyle("-fx-alignment: CENTER;");
+
+        // Create a cell value factory for the Mode Of Payment column
+        TableColumn<Bill, String> modeOfPaymentCol = new TableColumn<>("Mode of Payment");
+        modeOfPaymentCol.setCellValueFactory(new PropertyValueFactory<>("modeOfpayment"));
+        modeOfPaymentCol.setPrefWidth(170);
+        modeOfPaymentCol.setStyle("-fx-alignment: CENTER;");
+
+        // Create a cell value factory for the Status column
+        TableColumn<Bill, String> statusCol = new TableColumn<>("Order Status");
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
+        statusCol.setPrefWidth(170);
+
+        // Set the cell value factories for the table columns
+        billsTable.getColumns().setAll(billNoCol, dateCol, grandTotalCol, totalCol, modeOfPaymentCol, statusCol);
+        billsTable.setItems(thisYearsBills);
+        billsTable.getSortOrder().add(billNoCol);
+        billsTable.sort();
+
+        statusCol.setCellFactory(column -> new TableCell<Bill, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (item == null || empty) {
+                    setText("");
+                    setStyle("");
+                } else {
+                    setText(item);
+                    if (item.equals("SUCCESS")) {
+                        // Set the background color of the cell to green if status is SUCCESS
+                        setStyle("-fx-background-color: #c9f5c9;");
+                        setAlignment(Pos.CENTER);
+                    } else if (item.equals("IN PROGRESS")) {
+                        // Set the background color of the cell to yellow if status is IN PROGRESS
+                        setStyle("-fx-background-color: #dffc74;");
+                        setAlignment(Pos.CENTER);
+                    } else {
+                        // Set the background color of the cell to red if status is CANCELLED
+                        setStyle("-fx-background-color: #f5c9c9;");
+                        setAlignment(Pos.CENTER);
+                    }
+                }
+            }
+        });
     }
 
 
